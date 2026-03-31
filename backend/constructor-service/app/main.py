d222b75d6a10049118e1ab8811f3625dc6939195
Eaglet12IQ <eaglet12iq@gmail.com>
2026-03-31 15:34:42 +0300

    initial dota

---
+++ backend/constructor-service/app/main.py
@@ -0,0 +1,20 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from app.api.routers import api_router
+from app.core.git import get_repo
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
+    get_repo()          # инициализируем репозиторий один раз при старте
+    print("FastAPI + Git repo готов к работе")
+    yield
+    # Shutdown (если нужно что-то закрыть)
+    print("Приложение завершает работу")
+
+app = FastAPI(
+    title="constructor-service",
+    lifespan=lifespan
+)
+
+app.include_router(api_router, prefix="/api")
